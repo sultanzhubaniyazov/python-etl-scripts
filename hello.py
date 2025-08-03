4de3b0cf7d035df04e82c1e81ea1c4e27287f973
Sultan Zhubaniyazov <sultanzhubaniyazov04@gmail.com>
2025-08-03 23:35:08 +0300

    Добавил скрипт simple_txt_reader.py и файл heights

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print("Hello, GitHub!")
+# функция для классификакий роста
+def classify_height(h):
+    if h < 160:
+        return "Ниже среднего"
+    if 160 <= h <= 190:
+        return "Средний рост"
+    if h > 190:
+        return "Высокий рост"
+
+# читаем файл и переводим данные в тип данных int
+with open("heights", "r") as f:
+    array = [int(i.strip()) for i in f.readlines()]
+
+for h in array:
+    print(f"{h} - {classify_height(h)}")
